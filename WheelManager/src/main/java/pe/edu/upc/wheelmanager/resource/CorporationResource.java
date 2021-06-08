4e63d395e39675d8daff7c2cd25d7fab94251796
Jose <holzent@gmail.com>
2021-06-07 22:13:55 -0500

    Product Feature Creation
    Creation of product feature

--- WheelManager/src/main/java/pe/edu/upc/wheelmanager/resource/CorporationResource.java
+++ WheelManager/src/main/java/pe/edu/upc/wheelmanager/resource/CorporationResource.java
@@ -3,12 +3,6 @@ import lombok.Getter;
 import lombok.Setter;
 import pe.edu.upc.wheelmanager.domain.model.AuditModel;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Getter
 @Setter
---
+++ WheelManager/src/main/java/pe/edu/upc/wheelmanager/resource/SaveProductResource.java
@@ -0,0 +1,40 @@
+package pe.edu.upc.wheelmanager.resource;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class SaveProductResource {
+
+    @Column(unique = true)
+    private int rating;
+
+    @Column(unique = true)
+    private int units_int_stock;
+
+    @NotNull
+    @Size(max = 25)
+    @Column(unique = true)
+    private String name;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(unique = true)
+    private String description;
+
+    @Size(max = 30)
+    @Column(unique = true)
+    private String category;
+
+    @NotNull
+    @Size(max = 20)
+    @Column(unique = true)
+    private  Double price;
+
+    @Size(max = 200)
+    private String picture;
+}
